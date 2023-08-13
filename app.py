3c882dca9174961788ab2da0f64c310ef0aae5f2
DESKTOP-AN24RJL\nn <jossnghia@gmail.com>
2023-08-13 10:57:57 +0700

    edit readme

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = f"<h3>WELL COME MY Home</h3>"
+    html = f"<h3>WELL COME MY Home NGHIAHV1!</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
