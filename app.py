3ec47194384df8199c498306dcf220161a8a21b8
DESKTOP-AN24RJL\nn <jossnghia@gmail.com>
2023-08-23 07:08:16 +0700

    fix comment

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ LOG.setLevel(logging.INFO)
 
 def scale(payload):
     """Scales Payload"""
-    
+    load
     LOG.info(f"Scaling Payload: \n{payload}")
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
